cc6ab40225314359b6b34c6549fe106334dadf6b
hhaga <hakon.haga@bekk.no>
2023-12-13 14:45:39 +0100

    oh my zsh

---
+++ install_omz.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+if ! [[ -d ~/.oh-my-zsh ]]; then
+  echo "Installing oh-my-zsh"
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+#check if ~/.zshrc exists
+# if it does, remove it
+if [[ -f $HOME/.zshrc ]]; then
+  rm $HOME/.zshrc
+fi
+
+ln -s $HOME/Projects/dotfiles/oh-my-zsh-custom/zshrc $HOME/.zshrc
+
